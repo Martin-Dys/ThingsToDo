1316f8e3355774310cb3c8f82a084ef7ea7c1616
Martin Dys <martin.dys@edhec.com>
2019-02-21 14:33:41 +0100

    db

--- ThingsToDo/backoffice/models.py
+++ ThingsToDo/backoffice/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+class Task(models.Model):
+    name = models.CharField(max_length=20)
+    description = models.CharField(
+        max_length=200, 
+        blank=True,
+        null=True,)
+    priority = models.IntegerField(default=0)
+    start_date = models.DateTimeField('start date')
+    end_date = models.DateTimeField('end date')
+    place = models.CharField(
+        max_length=200, 
+        blank=True,
+        null=True,)
+    def __str__(self):
+        return self.name
+
+class Subtask(models.Model):
+    task = models.ForeignKey(Task, on_delete=models.CASCADE)
+    name = models.CharField(max_length=20)
+    description = models.CharField(
+        max_length=200,
+        blank=True,
+        null=True,)
+    order = models.IntegerField(default=1)
+    def __str__(self):
+        return self.name
