e496fc1d18c4a138daf8d1ec7d8f0245a291bb38
Martin Dys <martin.dys@edhec.com>
2019-02-21 16:37:42 +0100

    part3

--- ThingsToDo/backoffice/urls.py
+++ ThingsToDo/backoffice/urls.py
@@ -1,7 +1,15 @@
+
 from django.urls import path
 
 from . import views
 
 urlpatterns = [
+    # ex: /backoffice/
     path('', views.index, name='index'),
+    # ex: /backoffice/5/
+    path('<int:task_id>/', views.detail, name='detail'),
+    # ex: /backoffice/5/results/
+    path('<int:task_id>/results/', views.results, name='results'),
+    # ex: /backoffice/5/subtask/
+    path('<int:task_id>/subtask/', views.subtask, name='subtask'),
 ]
--- ThingsToDo/backoffice/views.py
+++ ThingsToDo/backoffice/views.py
@@ -1,5 +1,20 @@
-from django.http import HttpResponse
+from django.http import Http404
+from django.shortcuts import get_object_or_404, render
+from .models import Task, Subtask
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the index.")
+    latest_task_list = Task.objects.order_by('-start_date')[:10]
+    context = {'latest_task_list': latest_task_list}
+    return render(request, 'backoffice/index.html', context)
+
+def detail(request, task_id):
+    task = get_object_or_404(Task, pk=task_id)
+    return render(request, 'backoffice/detail.html', {'task': task})
+
+def results(request, task_id):
+    response = "You're looking at the results of task %s."
+    return HttpResponse(response % task_id)
+
+def subtask(request, task_id):
+    return HttpResponse("You're voting on task %s." % task_id)
